9f948fa5b55ed51dc599017838d744587ae371f8
Afsarud <engafsar5577@gmail.com>
2019-10-02 21:40:22 +0600

    ItemAssignment

---
+++ CoffeeShopAssignmentUI/BLL/ItemManager.cs
@@ -0,0 +1,47 @@
+﻿using CoffeeShopAssignmentUI.Model;
+using CoffeeShopAssignmentUI.Repository;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeShopAssignmentUI.BLL
+{
+    
+    public class ItemManager
+    {
+        ItemRepository _itemRepository = new ItemRepository();
+        public bool Add(Item item)
+        {
+            return _itemRepository.Add(item);
+
+        }
+        public bool IsNameExists(string name)
+        {
+            return _itemRepository.IsNameExists(name);
+        }
+        public DataTable Display()
+        {
+            return _itemRepository.Display();
+        }
+        public bool Delete(int ItemId)
+        {
+            return _itemRepository.Delete(ItemId);
+        }
+        public bool Update(string Name, double Price, int ItemId)
+        {
+            return _itemRepository.Update(Name, Price, ItemId);
+        }
+        public DataTable Search(string name)
+        {
+            return _itemRepository.Search(name);
+        }
+        public DataTable ItemCombobox()
+        {
+            return _itemRepository.ItemCombobox();
+        }
+
+    }
+}
---
+++ CoffeeShopAssignmentUI/ItemAssignmentUI.cs
@@ -0,0 +1,144 @@
+﻿using CoffeeShopAssignmentUI.BLL;
+using CoffeeShopAssignmentUI.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CoffeeShopAssignmentUI
+{
+    public partial class ItemAssignmentUI : Form
+    {
+        ItemManager _itemManager = new ItemManager();
+        Item _item = new Item();
+        public ItemAssignmentUI()
+        {
+            InitializeComponent();
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            _item.Name = nameTextBox.Text;
+            //Check UNIQUE
+            if (_itemManager.IsNameExists(_item.Name))
+            {
+                MessageBox.Show(nameTextBox.Text + "Already Exists!");
+                return;
+            }
+
+            //Set Price as Mandatory
+            if (String.IsNullOrEmpty(priceTextBox.Text))
+            {
+                MessageBox.Show("Price Can not be Empty!!!");
+                return;
+            }
+
+            _item.Price = Convert.ToDouble(priceTextBox.Text);
+
+            //Add/Insert Item
+            bool isAdded = _itemManager.Add(_item);
+
+            if (isAdded)
+            {
+                MessageBox.Show("Saved");
+            }
+            else
+            {
+                MessageBox.Show("Not Saved");
+            }
+
+            showDataGridView.DataSource = _itemManager.Display();
+            nameTextBox.Clear();
+            priceTextBox.Clear();
+            idtextBox.Clear();
+        }
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+            showDataGridView.DataSource = _itemManager.Display();
+            nameTextBox.Clear();
+            priceTextBox.Clear();
+            idtextBox.Clear();
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            //Set Id as Mandatory
+            if (String.IsNullOrEmpty(idtextBox.Text))
+            {
+                MessageBox.Show("Id Can not be Empty!!!");
+                return;
+            }
+
+            //Delete
+            if (_itemManager.Delete(Convert.ToInt32(idtextBox.Text)))
+            {
+                MessageBox.Show("Deleted");
+            }
+            else
+            {
+                MessageBox.Show("Not Deleted");
+            }
+
+            showDataGridView.DataSource = _itemManager.Display();
+            nameTextBox.Clear();
+            priceTextBox.Clear();
+            idtextBox.Clear();
+        }
+
+        private void updateButton_Click(object sender, EventArgs e)
+        {
+            //Set Id as Mandatory
+            if (String.IsNullOrEmpty(idtextBox.Text))
+            {
+                MessageBox.Show("Id Can not be Empty!!!");
+                return;
+            }
+            //Set Price as Mandatory
+            if (String.IsNullOrEmpty(priceTextBox.Text))
+            {
+                MessageBox.Show("Price Can not be Empty!!!");
+                return;
+            }
+            _item.Name = nameTextBox.Text;
+            _item.Price = Convert.ToDouble(priceTextBox.Text);
+            _item.ID = Convert.ToInt32(idtextBox.Text);
+            if (_itemManager.Update(_item.Name, _item.Price, _item.ID))
+            {
+                MessageBox.Show("Updated");
+                showDataGridView.DataSource = _itemManager.Display();
+            }
+            else
+            {
+                MessageBox.Show("Not Updated");
+            }
+        }
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            //Set Price as Mandatory
+            if (String.IsNullOrEmpty(nameTextBox.Text))
+            {
+                MessageBox.Show("Eield is Empty!");
+                return;
+            }
+
+            showDataGridView.DataSource =
+           _itemManager.Search(nameTextBox.Text);
+            nameTextBox.Clear();
+            priceTextBox.Clear();
+            idtextBox.Clear();
+        }
+
+        private void ItemAssignmentUI_Load(object sender, EventArgs e)
+        {
+            itemComboBox.DataSource = _itemManager.ItemCombobox();
+        }
+    }
+}
