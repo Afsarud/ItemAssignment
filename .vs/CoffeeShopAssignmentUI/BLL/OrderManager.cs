2038a115d3c2d0baeafc3c7cd8f6f93038891c2b
Afsarud <engafsar5577@gmail.com>
2019-10-03 07:17:57 +0600

    Order

---
+++ .vs/CoffeeShopAssignmentUI/BLL/OrderManager.cs
@@ -0,0 +1,25 @@
+﻿using CoffeeShopAssignmentUI.Model;
+using CoffeeShopAssignmentUI.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeShopAssignmentUI.BLL
+{
+   public class OrderManager
+    {
+        OrderRepository _orderRepository = new OrderRepository();
+        public DataTable GetAllItem()
+        {
+            return _orderRepository.GetAllItem();
+        }
+        public DataTable GetAllCustomer()
+        {
+            return _orderRepository.GetAllCustomer();
+        }
+
+    }
+}
