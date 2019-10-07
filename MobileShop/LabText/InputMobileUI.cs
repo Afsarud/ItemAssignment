67ee178f51f1ad32c760c25c6baa8c393f78ebd5
Afsarud <engafsar5577@gmail.com>
2019-10-07 19:43:48 +0600

    last07

--- MobileShop/LabText/InputMobileUI.cs
+++ MobileShop/LabText/InputMobileUI.cs
@@ -25,63 +25,60 @@ namespace LabText
             MobileManager _mobileManager = new MobileManager();
             Mobile _mobile = new Mobile();
 
-            //Mandetory
-            _mobile.Name = nameTextBox.Text;
-            if (string.IsNullOrEmpty(_mobile.Name))
+            try
             {
-                MessageBox.Show("Empty name not possible!!");
-                return;
+                //Mandetory
+                _mobile.Name = nameTextBox.Text;
+                if (string.IsNullOrEmpty(_mobile.Name))
+                {
+                    MessageBox.Show("Empty name not possible!!");
+                    return;
+                }
+
+                //Mandetory
+                _mobile.IMEI = imeiTextBox.Text;
+                if (string.IsNullOrEmpty(_mobile.IMEI))
+                {
+                    MessageBox.Show("Empty IMEI not possible!!");
+                    return;
+                }
+
+                if (_mobile.IMEI.Length != 11)
+                {
+                    MessageBox.Show("Please enter 11 digit !!");
+                    return;
+                }
+                //Unique
+                if (_mobileManager.IsEMEIExist(_mobile))
+                {
+                    MessageBox.Show(_mobile.IMEI + "Already Exist !!!");
+                    return;
+                }
+
+                //Mandetory
+                _mobile.Price = Convert.ToInt32(priceTextBox.Text);
+
+                if (string.IsNullOrEmpty(priceTextBox.Text))
+                {
+                    MessageBox.Show("Empty Price not possible!!");
+                    return;
+                }
+
+                if (_mobileManager.Add(_mobile))
+                {
+                    MessageBox.Show("Saved");
+                }
+
+                nameTextBox.Clear();
+                imeiTextBox.Clear();
+                priceTextBox.Clear();
+            }
+            catch (Exception exception)
+            {
+
+                MessageBox.Show(exception.Message);
             }
             
-            //Mandetory
-            _mobile.IMEI = imeiTextBox.Text;
-            if (string.IsNullOrEmpty(_mobile.IMEI))
-            {
-                MessageBox.Show("Empty IMEI not possible!!");
-                return;
-            }
-            
-            if (_mobile.IMEI.Length != 11)
-            {
-                MessageBox.Show("Please enter 11 digit !!");
-                return;
-            }
-
-            if (_mobileManager.IsEMEIExist(_mobile))
-            {
-                MessageBox.Show(_mobile.IMEI + "Already Exist !!!");
-                return;
-            }
-
-            // unique
-            //_item.Name = nameTextBox.Text;
-            ////Check UNIQUE
-            //if (_itemManager.IsNameExists(_item))
-            //{
-            // MessageBox.Show(nameTextBox.Text + "Already Exists!");
-            // return;
-            //}
-
-
-            //Mandetory
-            _mobile.Price = Convert.ToInt32(priceTextBox.Text);
-            
-            if (string.IsNullOrEmpty(priceTextBox.Text))
-            {
-                MessageBox.Show("Empty Price not possible!!");
-                return;
-            }
-
-            //bool IsInsert = _mobileManager.Add(_mobile);
-
-            if (_mobileManager.Add(_mobile))
-            {
-                MessageBox.Show("Saved");
-            }
-
-            nameTextBox.Clear();
-            imeiTextBox.Clear();
-            priceTextBox.Clear();
         }
     }
 }
--- MobileShop/LabText/Repository/MobileRepository.cs
+++ MobileShop/LabText/Repository/MobileRepository.cs
@@ -13,55 +13,69 @@ namespace LabText.Repository
     class MobileRepository
     {
        public bool Add(Mobile mobile)
-        {
-            bool IsAdded = false;
-            //Connection 
-            string connectionString = @"server = DESKTOP-GIE8L6J; Database= MobileShop; Integrated Security= True ";
-            SqlConnection sqlConnection = new SqlConnection(connectionString);
-
-            // Command 
-            string commandString = @"insert into Mobile values('"+ mobile.Name+ "','"+mobile.IMEI+"','"+mobile.Price+"')";
-            SqlCommand sqlCommand = new SqlCommand(commandString, sqlConnection);
-
-            sqlConnection.Open();
-
-
-            int isExecuted = sqlCommand.ExecuteNonQuery();
-
-            if (isExecuted > 0)
             {
-                IsAdded = true;
+                bool IsAdded = false;
+                    try
+                        {
+                            //Connection 
+                            string connectionString = @"server = DESKTOP-GIE8L6J; Database= MobileShop; Integrated Security= True ";
+                            SqlConnection sqlConnection = new SqlConnection(connectionString);
+
+                            // Command 
+                            string commandString = @"insert into Mobile values('" + mobile.Name + "','" + mobile.IMEI + "','" + mobile.Price + "')";
+                            SqlCommand sqlCommand = new SqlCommand(commandString, sqlConnection);
+
+                            sqlConnection.Open();
+
+
+                            int isExecuted = sqlCommand.ExecuteNonQuery();
+
+                            if (isExecuted > 0)
+                            {
+                                IsAdded = true;
+                            }
+                            sqlConnection.Close();
+                
+                        }
+                        catch (Exception exception)
+                        {
+
+                            MessageBox.Show(exception.Message);
+                        }
+                return IsAdded;
             }
-            sqlConnection.Close();
-            return IsAdded;
-        }
 
         public bool IsEMEIExist(Mobile mobile)
         {
-            
             bool IsExist = false;
+                try
+                {
+                    //Connection 
+                    string connectionString = @"server = DESKTOP-GIE8L6J; Database= MobileShop; Integrated Security= True ";
+                    SqlConnection sqlConnection = new SqlConnection(connectionString);
 
-            //Connection 
-            string connectionString = @"server = DESKTOP-GIE8L6J; Database= MobileShop; Integrated Security= True ";
-            SqlConnection sqlConnection = new SqlConnection(connectionString);
+                    // Command 
 
-            // Command 
-            
-            string commandString = @"select * from Mobile where IMEI = '"+mobile.IMEI+"'";
-            SqlCommand sqlCommand = new SqlCommand(commandString, sqlConnection);
+                    string commandString = @"select * from Mobile where IMEI = '" + mobile.IMEI + "'";
+                    SqlCommand sqlCommand = new SqlCommand(commandString, sqlConnection);
 
-            sqlConnection.Open();
-            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand);
-            DataTable dataTable = new DataTable();
-            
-            sqlDataAdapter.Fill(dataTable);
-            sqlConnection.Close();
-            if (dataTable.Rows.Count > 0)
-            {
-                IsExist = true;
-            }
-            
-            
+                    sqlConnection.Open();
+                    SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand);
+                    DataTable dataTable = new DataTable();
+
+                    sqlDataAdapter.Fill(dataTable);
+                    sqlConnection.Close();
+                    if (dataTable.Rows.Count > 0)
+                    {
+                        IsExist = true;
+                    }
+                
+                }
+                catch (Exception exception)
+                {
+
+                    MessageBox.Show(exception.Message);
+                }
             return IsExist;
         }
     }
